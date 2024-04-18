ea1bc0b6e6ae862e0a96e8e002cd53198b17eeae
Virendra1306 <virendrasinghnegi0123@gmail.com>
2024-04-19 03:47:09 +0530

    Updated

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+import { StateProvider } from "./components/context/StateProvider";
+import { initialState } from "./components/context/initialState";
+import reducer from "./components/context/reducer";
+
+import './index.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </BrowserRouter>
+, document.getElementById("root"));
